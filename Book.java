553b762eeeb7b5a44a83c45a4fac902357d8de7b
MannyASC <emanthegreat17@icloud.com>
2025-04-01 13:53:51 -0400

    progress on book class

--- Book.java
+++ Book.java
@@ -1,5 +1,28 @@
 public class Book{
-    //Book Variables
+    
     private int isbn;
     private String title, author;
+    private boolean isAvailable;
+    private int copies;
+
+    public Book(int isbn, String title, String author, int copies) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.copies = copies;
+        this.isAvailable = copies > 0;
+    }
+
+    //get and set mthodes 
+    public int getIsbn() { return isbn; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isAvailable() { return isAvailable; }
+    public int getCopies() { return copies; }
+    
+    public void setIsAvailable(boolean available) { isAvailable = available; }
+    public void setCopies(int copies) { 
+        this.copies = copies;
+        this.isAvailable = copies > 0;
+    }
 }
