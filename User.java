12738b0b86c48aed06cbfd1edf45d3b2c9bdc376
MannyASC <emanthegreat17@icloud.com>
2025-04-02 10:11:40 -0400

    iprovements to user class

--- User.java
+++ User.java
@@ -15,14 +15,40 @@ public class User{
     }
 
     //get methods
-    public int getId() { return this.id; }
-    public String getName() { return this.name; }
-    public ArrayList<Book> getBooksBorrowed() { return this.booksBorrowed; }
-    public double getFine() { return this.fine; }
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public ArrayList<Book> getBooksBorrowed() { return booksBorrowed; }
+    public double getFine() { return fine; }
 
     //set methods
     public void setId(int id){ this.id = id; }
     public void setName(String name){ this.name = name; }
     public void setBooksBorrowed(ArrayList<Book> booksBorrowed){ this.booksBorrowed = booksBorrowed; }
     public void setFine(double fine){ this.fine = fine; }
+
+    public void borrowBook(Book book) {
+        booksBorrowed.add(book);
+    }
+
+    public void returnBook(Book book) {
+        for (int i = 0; i < booksBorrowed.size(); i++) {
+            if (booksBorrowed.get(i).getIsbn()==(book.getIsbn())) {
+                booksBorrowed.remove(i);
+                break;
+            }
+        }
+    }
+    public int getBorrowedBooksCount() {
+        return booksBorrowed.size();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", borrowedBooks=" + booksBorrowed.size() +
+                ", fine=" + fine +
+                '}';
+    }
 }
