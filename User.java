604201c66f21a598c8c65a48319f7f0ab35d553e
nateeea <nathanbru21@gmail.com>
2025-04-01 21:22:46 -0400

    Removed an arbitrary constructor and added a new fine variable and methods for it

--- User.java
+++ User.java
@@ -3,26 +3,26 @@ public class User{
     //user variables
     private int id;
     private String name;
+    private int fine;
     private ArrayList<Book> booksBorrowed;
 
-    //constructors
+    //constructor
     public User(int id, String name){
         this.id = id;
         this.name = name;
+        this.fine = 0;
         this.booksBorrowed = new ArrayList<>();
-    }
-    public User(int id, String name,ArrayList<Book> booksBorrowed){
-        this.id = id;
-        this.name = name;
-        this.booksBorrowed = booksBorrowed;
     }
 
     //get methods
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public ArrayList<Book> getBooksBorrowed() { return booksBorrowed; }
+    public int getId() { return this.id; }
+    public String getName() { return this.name; }
+    public ArrayList<Book> getBooksBorrowed() { return this.booksBorrowed; }
+    public int getFine() { return this.fine; }
 
     //set methods
     public void setId(int id){ this.id = id; }
     public void setName(String name){ this.name = name; }
+    public void setBooksBorrowed(ArrayList<Book> booksBorrowed){ this.booksBorrowed = booksBorrowed; }
+    public void setFine(int fine){ this.fine = fine; }
 }
