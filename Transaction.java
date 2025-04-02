7b91fa82f8e2658deb0d5084d56e29bdde7327e6
nateeea <nathanbru21@gmail.com>
2025-04-02 13:50:24 -0400

    Added numTransactions variable and some comments

--- Transaction.java
+++ Transaction.java
@@ -1,26 +1,39 @@
 import java.time.LocalDate;
 
 public class Transaction {
+
+    //transaction variables
     private int transactionId;
     private User user;
     private Book book;
     private LocalDate borrowDate;
     private LocalDate returnDate;
     private double fine;
+    //acts as transactionId
+    private static int numTransactions = 0;
 
-    public Transaction(int transactionId, User user, Book book) {
-        this.transactionId = transactionId;
+    //constructor for current date
+    public Transaction(User user, Book book) {
+        this.transactionId = numTransactions;
         this.user = user;
         this.book = book;
         this.borrowDate = LocalDate.now();
         this.fine = 0.0;
+        numTransactions++;
     }
 
-    public Transaction(int transactionId, User user, Book book, LocalDate borrowDate) {
-        this.transactionId = transactionId;
+    //constructor for custom date
+    public Transaction(User user, Book book, LocalDate borrowDate) {
+        this.transactionId = numTransactions;
         this.user = user;
         this.book = book;
         this.borrowDate = borrowDate;
         this.fine = 0.0;
+        numTransactions++;
     }
+
+    //Implement getTransaction when we figure out file system
+    /*public static Transaction getTransaction(int transactionId){
+
+    }*/
 }
