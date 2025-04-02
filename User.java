2e181d2d93bafe1ab15d514d3a7471105edb2e1e
nateeea <nathanbru21@gmail.com>
2025-04-02 10:47:42 -0400

    Seperated name into first and last name

--- User.java
+++ User.java
@@ -2,27 +2,32 @@ import java.util.ArrayList;
 public class User{   
     //user variables
     private int id;
-    private String name;
+    private String fname;
+    private String lname;
     private double fine;
     private ArrayList<Book> booksBorrowed;
 
     //constructor
-    public User(int id, String name){
+    public User(int id, String fname,String lname){
         this.id = id;
-        this.name = name;
+        this.fname = fname;
+        this.lname = lname;
         this.fine = 0;
         this.booksBorrowed = new ArrayList<>();
     }
 
     //get methods
     public int getId() { return id; }
-    public String getName() { return name; }
+    public String getFirstName() { return fname; }
+    public String getLastName() { return lname; }
+    public String getName() { return fname + " " + lname; }
     public ArrayList<Book> getBooksBorrowed() { return booksBorrowed; }
     public double getFine() { return fine; }
 
     //set methods
     public void setId(int id){ this.id = id; }
-    public void setName(String name){ this.name = name; }
+    public void setFirstName(String name){ this.fname = name; }
+    public void setLastName(String name){ this.lname = name; }
     public void setBooksBorrowed(ArrayList<Book> booksBorrowed){ this.booksBorrowed = booksBorrowed; }
     public void setFine(double fine){ this.fine = fine; }
 
@@ -51,7 +56,7 @@ public class User{
     public String toString() {
         return "User: {" +
                 "id='" + id + '\'' +
-                ", name='" + name + '\'' +
+                ", name='" + getName() + '\'' +
                 ", borrowedBooks=" + booksBorrowed.size() +
                 ", fine=" + fine +
                 '}';
