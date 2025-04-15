590765690bbc5e11f37ddb73ac2e9f60b2ea6f85
nateeea <nathanbru21@gmail.com>
2025-04-15 19:56:01 -0400

    added a constructor for the FileManager class

--- FileManager.java
+++ FileManager.java
@@ -1,6 +1,14 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 public class FileManager {
-    Connection con = DriverManager.getConnection();
-
+    public Connection con;
+    public FileManager(){          
+        try {
+            con = DriverManager.getConnection("jbdc:sqlite:Files.db");
+            System.out.println("Connection to the database has been established");
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+    }
 }
